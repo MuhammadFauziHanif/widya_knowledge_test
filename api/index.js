a6f877538ce09ab1307538fe9d3770982a027e14
MuhammadFauziHanif <muhammad.hanif010@binus.ac.id>
2023-09-07 23:01:27 +0700

    add index.js in api

---
+++ api/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const db = require('../config/db');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+const authRoutes = require('../app/routes/authRoutes');
+const userRoutes = require('../app/routes/userRoutes');
+
+app.use('/auth', authRoutes);
+app.use('/user', userRoutes);
+
+// Database connection
+db.authenticate()
+  .then(() => {
+    console.log('Database connected');
+  })
+  .catch(err => {
+    console.error('Error connecting to database:', err);
+  });
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});

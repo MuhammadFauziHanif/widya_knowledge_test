a5553b17d3b594c2ebd9117409dd4434ebc1b418
MuhammadFauziHanif <muhammad.hanif010@binus.ac.id>
2023-09-08 15:11:50 +0700

    Migrate DB to Railway

--- config/db.js
+++ config/db.js
@@ -3,12 +3,8 @@ require('dotenv').config(); // Load environment variables from .env file
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: process.env.DB_DIALECT,
-  dialectOptions: {
-    ssl: {
-      sslmode: require,
-    },
-  },
 });
 
 module.exports = db;

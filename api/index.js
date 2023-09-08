181d4747d6f868f925b44ad9bc02ff0d656f03f3
MuhammadFauziHanif <muhammad.hanif010@binus.ac.id>
2023-09-08 12:58:47 +0700

    change port

--- api/index.js
+++ api/index.js
@@ -25,5 +25,5 @@ db.authenticate()
 let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on port ${port}`);
 });
